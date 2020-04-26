697954d3395cd64e38ba65ab4e460b555409dcf7
LJ Spencer <hockeyduck3@gmail.com>
2020-04-25 18:13:52 -0600

    Added some of the functionality needed

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const questions = [
+const fs = require("fs")
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
+const questions = [
+    {
+        type: 'input',
+        message: 'What is name of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Make a short description for your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What would you like in your table of contents?',
+        name: 'table'
+    }
 ];
 
 function writeToFile(fileName, data) {
+    let generated = generateMarkdown(data);
+
+    fs.writeFile(fileName, generated, () => {
+        console.log(`${fileName} has been generated!`)
+    })
 }
 
 function init() {
+    inquirer.prompt(questions).then(function (answers) {
+        console.log(answers);
 
+        writeToFile(`${answers.title}-README.md`, answers);
+    });
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,15 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+## Description
+
+${data.description}
+
+
+## Table of contents
+
+* ${data.table}
+
 `;
 }
 
